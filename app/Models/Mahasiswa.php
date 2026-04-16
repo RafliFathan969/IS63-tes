9d2181e5b9f694d5f1cce982d20ddd43b56d7d6a
RafliFathan969 <kyzenn40@gmail.com>
2026-04-16 12:13:52 +0700

    pertemuan 4

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    //use HasFactory;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'jurusan',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'nohp',
+        'domisili',
+        'email',
+        'jenis_kelamin',
+        'tahun_masuk',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contoh1', function () {
+    return view('lp3i');
+});
+
+Route::get('/master', function () {
+    return view('layout.master');
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'create']);
